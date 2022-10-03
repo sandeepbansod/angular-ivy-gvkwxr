8266821e0db904ce22aed67cca54416630a817d6
sandeepbansod <46375120+sandeepbansod@users.noreply.github.com>
2022-10-04 00:22:27 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,36 @@
-import { Component, VERSION, OnInit } from '@angular/core';
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = 'Angular';
 
-ngOnInit(){
+  ngOnInit() {
+    //// Observable
+    const myObs = new Observable((observable) => {
+      observable.next('Call Observable Now.');
+    });
 
-  //// Observable
-  const myObs = new Observable((observable)=>{
-    observable.next("Call Observable Now.");
-  });
+    console.log('Call Observable Before');
 
-  console.log("Call Observable Before");
+    myObs.subscribe({
+      next: console.log,
+    });
+    console.log('Call Observable After');
 
-  myObs.subscribe({
-    next: console.log
-  });
-  console.log("Call Observable After");
+    ///// Promise
+    const myProm = new Promise((resolve, reject) => {
+      console.log('Call Promise Now.');
+      resolve('Promise Call Resolve');
+      reject('Promise Call Reject');
+    });
 
-  
-  ///// Promise
-  const myProm = new Promise((resolve, reject)=>{
-    console.log("Call Promise Now.");
-    resolve("Promise Call Resolve");
-    reject("Promise Call Reject");
-  });
-
-  myProm.then(res => {
-    console.log(res)
-  })
-
-
-
-}
-
+    myProm.then((res) => {
+      console.log(res);
+    });
+  }
 }
