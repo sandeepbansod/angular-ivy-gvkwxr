98944ed333b171842fe4b8ec969b43ecb783db1f
sandeepbansod <46375120+sandeepbansod@users.noreply.github.com>
2022-10-04 00:21:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-gvkwxr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gvkwxr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, VERSION, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  name = 'Angular ' + VERSION.major;
+
+ngOnInit(){
+
+  //// Observable
+  const myObs = new Observable((observable)=>{
+    observable.next("Call Observable Now.");
+  });
+
+  console.log("Call Observable Before");
+
+  myObs.subscribe({
+    next: console.log
+  });
+  console.log("Call Observable After");
+
+  
+  ///// Promise
+  const myProm = new Promise((resolve, reject)=>{
+    console.log("Call Promise Now.");
+    resolve("Promise Call Resolve");
+    reject("Promise Call Reject");
+  });
+
+  myProm.then(res => {
+    console.log(res)
+  })
+
+
+
+}
+
+}
